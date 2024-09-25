6f45df044b99a2c853fabcb3d2aec4a35e4993a5
nigel <nghia.thanh.nguyen@skymavis.com>
2024-09-25 15:35:13 +0700

    update

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,38 +1,31 @@
 import { Button } from "antd";
 import { useAtom, useSetAtom } from "jotai";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 import { isHostAtom, roomIdAtom } from "../store/room";
 import { request } from "../utils/request";
 import openSocket from "socket.io-client";
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const TURN_API_KEY = import.meta.env.VITE_TURN_API_KEY;
+const TURN_APP_NAME = import.meta.env.VITE_TURN_APP_NAME;
+const TURN_USER_NAME = import.meta.env.VITE_TURN_USER_NAME;
 const socket = openSocket(`${SERVER_URL}`);
 
 const config = {
-  iceServers: [],
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun.l.google.com:5349" },
+    { urls: "stun:stun1.l.google.com:3478" },
+    { urls: "stun:stun1.l.google.com:5349" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:5349" },
+    { urls: "stun:stun3.l.google.com:3478" },
+    { urls: "stun:stun3.l.google.com:5349" },
+    { urls: "stun:stun4.l.google.com:19302" },
+    { urls: "stun:stun4.l.google.com:5349" },
+  ],
 };
-
-const pc = new RTCPeerConnection({ ...config, iceTransportPolicy: "relay" });
-
-(async () => {
-  const res = await fetch("https://global.xirsys.net/_turn/owwirtc", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Basic " + btoa("javthanhnghia:4ea4b994-771f-11ef-932c-0242ac130002"),
-    },
-    body: JSON.stringify({ format: "urls" }),
-  });
-  const data = await res.json();
-  config.iceServers = data.v.iceServers;
-  const configs = { ...pc.getConfiguration() };
-  configs.iceServers = [data.v.iceServers];
-  pc.setConfiguration(configs);
-})();
-
-console.log({ getConfiguration: pc.getConfiguration() });
 
 const userName = `USER-${Math.floor(Math.random() * 1000)}`;
 
@@ -41,6 +34,7 @@ let done = false;
 export const RoomPage = () => {
   const { id: roomId } = useParams();
   const navigate = useNavigate();
+  const pc = useMemo(() => new RTCPeerConnection({ ...config, iceTransportPolicy: "relay" }), []);
 
   const [isHost, setIsHost] = useAtom(isHostAtom);
   const setRoomId = useSetAtom(roomIdAtom);
@@ -69,7 +63,7 @@ export const RoomPage = () => {
     // 4. The caller calls RTCPeerConnection.setLocalDescription() to set that offer as the local description (that is, the description of the local end of the connection).
     await pc.setLocalDescription(offer);
     done = true;
-  }, []);
+  }, [pc]);
 
   const joinCall = useCallback(async () => {
     if (!localVideo.current) return;
@@ -96,7 +90,7 @@ export const RoomPage = () => {
       )
     );
     done = true;
-  }, [roomId]);
+  }, [roomId, pc]);
 
   useEffect(() => {
     if (!done) {
@@ -112,17 +106,21 @@ export const RoomPage = () => {
     pc.onicecandidate = async (event) => {
       if (event.candidate) {
         const candidate = event.candidate;
+        console.log({ oncandidate: candidate });
         setCandidates((prev) => [...prev, JSON.stringify(candidate)]);
       }
     };
-  }, []);
+    return () => {
+      pc.close();
+    };
+  }, [pc]);
 
-  useEffect(()=>{
+  useEffect(() => {
     socket.on("user-answer", async (data) => {
       console.log("answer data: ", data);
       const { answer, candidates } = data;
       console.log("Got answer from user", { answer, candidates });
-  
+
       if (candidates.length === 0) {
         console.log("EMPTY CANDIDATES");
         setTimeout(() => {
@@ -131,11 +129,11 @@ export const RoomPage = () => {
         }, 1000);
         return;
       }
-  
+
       if (!pc.remoteDescription) {
         await pc.setRemoteDescription(answer);
       }
-  
+
       await Promise.all(
         candidates.map(
           async (candidate: string) =>
@@ -145,9 +143,15 @@ export const RoomPage = () => {
         )
       );
       setIsWaitingPeer(false);
+      pc.getStats().then((stats) => {
+        stats.forEach((report) => {
+          console.log("report: ",{ ...report });
+        });
+      });
+
       console.log(pc);
     });
-  },[roomId])
+  }, [roomId, pc]);
 
   useEffect(() => {
     pc.onicegatheringstatechange = () => {
@@ -168,7 +172,7 @@ export const RoomPage = () => {
       }
     };
     console.log({ candidates });
-  }, [isHost, roomId, answer, offer, candidates]);
+  }, [isHost, roomId, answer, offer, candidates, pc]);
 
   // Update remote video when a new track is added
   useEffect(() => {
@@ -181,7 +185,31 @@ export const RoomPage = () => {
     pc.onsignalingstatechange = (event) => {
       console.log("signaling-state", event);
     };
-  }, [remoteVideo]);
+  }, [remoteVideo, pc]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        `https://global.xirsys.net/_turn/${TURN_APP_NAME}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Basic " + btoa(`${TURN_USER_NAME}:${TURN_API_KEY}`),
+          },
+          body: JSON.stringify({ format: "urls" }),
+        }
+      );
+      const data = await res.json();
+      config.iceServers = data.v.iceServers;
+      const configs = { ...pc.getConfiguration() };
+      if (!data.v?.iceServers) return;
+      configs.iceServers = [data.v.iceServers];
+      pc.setConfiguration(configs);
+    })();
+
+    console.log({ getConfiguration: pc.getConfiguration() });
+  }, []);
 
   if (!roomId) {
     navigate("/404");
